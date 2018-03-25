ed239c29a623f33e9b4077f9ea04db13e78d458d
sansan <b810521@gmail.com>
2018-03-25 16:17:15 +0800

    generate admin::categories controller and setup authorization

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,10 @@
+class Admin::CategoriesController < ApplicationController
+  #產生 Admin::CategoriesController 之後，請你立刻完成認證使用者權限的設定
+  before_action :authenticate_user!
+  #驗證請求進入後台的是否為已登入的 User
+  before_action :authenticate_admin
+  #驗證該 User 身份是否為網站管理員
+end
+
+#ApplicationController已經有authenticate_admin了，
+#而Admin::CategoriesController繼承了ApplicationController所以就不用再定義一次authenticate_admin
