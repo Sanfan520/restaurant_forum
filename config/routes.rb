0f72a21198f4da4ddbed580de4950395c113f3f8
sansan <b810521@gmail.com>
2018-04-24 22:06:35 +0800

    Config routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+ #Devise自動建立通往認證使用者的功能
+ devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ #前台首頁
  resources :restaurants, only: [:index, :show]
  root "restaurants#index"
 
-
+#後台功能:Restaurant CRUD & Categories CRUD以及後台首頁
 namespace :admin do
   resources :restaurants
   resources :categories
