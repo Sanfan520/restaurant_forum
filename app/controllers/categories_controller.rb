83b18de8a693b89827c09c00b1188d0f6acc4a00
sansan <b810521@gmail.com>
2018-04-29 15:36:34 +0800

    add show action to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,7 @@
 class CategoriesController < ApplicationController
+
+  def show
+    @categories = Category.all #上方分類標籤列——所有的分類資料
+    @category = Category.find(params[:id]) #被點擊的分類——特定一筆分類資料
+    @restaurants = @categories.restaurants.page(params[:page].per(9)#和該分類關聯的餐廳資料
 end
