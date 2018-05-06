41c76e0620561fd0a46844db81bb0a43ebce626b
sansan <b810521@gmail.com>
2018-05-06 21:33:45 +0800

    add user show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
    resources :comments, only: [:create, :destroy]
   end
 
-resources :comments,only: [:show, :edit, :update]
+resources :users, only: [:show, :edit, :update]
 
 
  resources :categories,only: :show
