5f2575db41e30658138824e7736c4514351059b5
sansan <b810521@gmail.com>
2018-05-19 21:44:04 +0800

    user can view commented restaurants on profile page, styled profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
 
@@ -12,4 +12,5 @@ class User < ApplicationRecord
   end
   #用 self 呼叫物件自己，而 self.role == "admin" 是一行陳述，如果 role 屬性等於 "admin"，該陳述就會回傳 True，否則將回傳 False。
   has_many :comments
+  has_many :restaurants, through: :comments
 end
