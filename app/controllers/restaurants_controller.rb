9a42d0c0ad11943f406b411a44159720e5888ba2
sansan <b810521@gmail.com>
2018-05-20 20:34:36 +0800

    user can view recent restaurants and comments on feeds page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,4 +11,10 @@ class RestaurantsController < ApplicationController
    @comment = Comment.new #輸入框有使用到 @comment
  end
 
+ #"餐廳最新動態"頁面功能
+  def feeds
+   @recent_restaurants = Restaurant.order(created_at: :desc).limit(10) #最新建立的 10 筆餐廳 - @recent_restaurants
+   @recent_comments = Comment.order(created_at: :desc).limit(10)#最新建立的 10 筆評論 - @recent_comments
+  end
+
 end
