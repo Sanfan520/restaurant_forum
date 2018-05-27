c9106f9a782ee8b193743c3591fff82499acfb61
sansan <b810521@gmail.com>
2018-05-27 14:03:37 +0800

    setup relation among user,restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,7 +7,13 @@ class Restaurant < ApplicationRecord
   #上一步private method 宣告restaurant_params方法後，會想要在model加入驗證步驟，
   #將name 欄位設定為必填。若使用者沒有填寫餐廳的名稱，就按下送出鈕，我們不會將這筆資料送入資料庫。
   belongs_to :category
-  
+
   #當 Restaurant 物件被刪除時，順便刪除依賴的 Comment
   has_many :comments, dependent: :destroy
+
+  # 「餐廳被很多使用者收藏」的多對多關聯
+  # 將關聯名稱自訂為 :favorited_users
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users,through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
-  mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, AvatarUploaders
 
   def admin?
     self.role == "admin"
   end
   #用 self 呼叫物件自己，而 self.role == "admin" 是一行陳述，如果 role 屬性等於 "admin"，該陳述就會回傳 True，否則將回傳 False。
-  # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
-  has_many :comments, dependent: :restrict_with_error
-  has_many :restaurants, through: :comments
+
+# 「使用者評論很多餐廳」的多對多關聯
+  has_many :comments, dependent: :restrict_with_error   # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
+  has_many :commented_restaurants, through: :comments, source: :restaurant
+
+# 「使用者收藏很多餐廳」的多對多關聯
+  # 「使用者收藏很多餐廳」的多對多關聯
+  # 由於使用 restaurants 會和「使用者評論很多餐廳」重覆，將關聯名稱自訂為 :favorited_restaurants
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :favorites, dependent: :destroy
+  has_many :favorites_restaurants, through: :favorites, source: :restaurant
+
 end
