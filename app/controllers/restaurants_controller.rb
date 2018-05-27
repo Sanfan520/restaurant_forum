475587a411ff698bce053d3df1ff15c3d9168dfa
sansan <b810521@gmail.com>
2018-05-28 00:26:24 +0800

    add favorite/unfavorite action on RestaurantsController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -21,4 +21,18 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  #新增收藏紀錄
+  def favorite
+    @restaurant = Restaurant.find(params[:id]) #依params[:id]確認物件
+    @restaurant.favorites.create!(user: current_user)#在favorites資料表上建立一筆新紀錄，並寫入restaurant_id 和user_id
+    redirect_back(fallback_location: root_path)#導回上一頁
+  end
+
+   #移除收藏紀錄
+   def unfavorite
+     @restaurant = Restaurant.find(params[:id]) #依 params[:id]確認餐廳物件
+     favorites = Favorite.where(restaurant: @restaurant, user: current_user)#根據餐廳物件和current_user 查詢出favorites 資料表的紀錄
+     favorites.destroy_all##將相關的紀錄刪除
+     redirect_back(fallback_location: root_path)#導回上一頁
+   end
 end
