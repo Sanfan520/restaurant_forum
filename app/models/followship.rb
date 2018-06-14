2955c0487672f41122a3bb11a1407bdd8c90e093
sansan <b810521@gmail.com>
2018-06-14 23:06:43 +0800

    validate uniqueness of following_id in user_id scope

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,11 @@
 class Followship < ApplicationRecord
+  # 每個 User 只能追蹤另一個 User 一次 ，從資料表的角度=確保特定 user_id 下，只能有一個 followings_id
+  validates :following_id, uniqueness: { scope: :user_id }
+
+
+  # 「使用者追蹤使用者」的 self-referential relationships 設定
+  #  由於 :following 指向 User Model, Rails 無法自動推論
+  #  使用 :class_name 告知對應的 Model 名稱
   belongs_to :user
   belongs_to :following, class_name: "User"
 end
